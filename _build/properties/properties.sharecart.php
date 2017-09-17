32b432d5078b4ba830bab7a8484c512e925f4ecc
Electrica <electricaev@gmail.com>
2017-09-17 16:27:53 +0600

    Version 1.0.0-beta

--- _build/properties/properties.sharecart.php
+++ _build/properties/properties.sharecart.php
@@ -7,30 +7,6 @@ $tmp = array(
         'type' => 'textfield',
         'value' => 'tpl.shareCart.item',
     ),
-    'sortby' => array(
-        'type' => 'textfield',
-        'value' => 'name',
-    ),
-    'sortdir' => array(
-        'type' => 'list',
-        'options' => array(
-            array('text' => 'ASC', 'value' => 'ASC'),
-            array('text' => 'DESC', 'value' => 'DESC'),
-        ),
-        'value' => 'ASC',
-    ),
-    'limit' => array(
-        'type' => 'numberfield',
-        'value' => 10,
-    ),
-    'outputSeparator' => array(
-        'type' => 'textfield',
-        'value' => "\n",
-    ),
-    'toPlaceholder' => array(
-        'type' => 'combo-boolean',
-        'value' => false,
-    ),
 );
 
 foreach ($tmp as $k => $v) {
